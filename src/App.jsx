dd1a60f8c0a4e1734fcb07c044950302d31af6bb
Umair <rajaumairkhalid555@gmail.com>
2023-07-28 09:15:05 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="mx-auto bg-section1 w-full">
+      <div className="max-w-6xl mx-auto bg-cyan-100">Everything goes here</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        blackBtn: '#161616',
+        fontAccent: '#ff4d30',
+        active: '##FF4C30',
+        shadow: 'rgba(255,83,48,.35)',
+        Dot: '#bbb6cb',
+        activeDot: '#ff6f1e',
+        backgroundLight: '#f8f8f8',
+        backgroundDark: '#2d2d2d',
+        'testimonials-bg': '#f9f9f9',
+      },
+      fontFamily: {
+        sans: ['Rubik', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
